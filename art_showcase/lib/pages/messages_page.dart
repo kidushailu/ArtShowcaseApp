00915ed9f311be79602b5d304dc05a7bd70ca5cd
kidushailu <hailu.kidus1@gmail.com>
2024-07-23 01:00:02 -0400

    Builds a list of users that can be messaged. Clicking a user will open up chat page between clicked user and current user.

---
+++ art_showcase/lib/pages/messages_page.dart
@@ -0,0 +1,70 @@
+import 'chat_page.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../helpers/bottom_bar.dart';
+
+class MessagePage extends StatefulWidget {
+  @override
+  _MessagePageState createState() => _MessagePageState();
+}
+
+class _MessagePageState extends State<MessagePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Messages'),
+      ),
+      body: _buildUserList(),
+      bottomNavigationBar: const BottomBar(),
+    );
+  }
+
+  // build a list of users except current user
+  Widget _buildUserList() {
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('users').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text("error");
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading...");
+          }
+
+          return ListView(
+            children: snapshot.data!.docs
+                .map<Widget>((doc) => _buildUserListItem(doc))
+                .toList(),
+          );
+        });
+  }
+
+  // build individual user list items
+  Widget _buildUserListItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+    //display all users except current user
+    if (_auth.currentUser!.email != data['email']) {
+      return ListTile(
+        title: Text(data['username']),
+        onTap: () {
+          // pass the clicked user's uid to the chat page
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ChatPage(
+                        receiverUsername: data['username'],
+                        receiverUserID: data['uid'],
+                      )));
+        },
+      );
+    } else {
+      return Container();
+    }
+  }
+}
